3281817e0d98ab82214ca20d873eec40f1d8e108
Jordan <jordmax12@gmail.com>
2021-04-02 17:40:15 -0500

    added comment

--- application/v1/controller/middleware.js
+++ application/v1/controller/middleware.js
@@ -2,6 +2,7 @@ const { get_api_key } = require('../model/api_key');
 
 exports.authenicate_api_key = async (req, res, next) => {
     const { api_key } = req.headers;
+    // NOTE: I usually would use something like Cognito or generate a JWT, but for the sake of time did a simple API key implementation.
     const validate_api_key = await get_api_key(api_key);
     if(validate_api_key.length === 0) {
         res.status(401);
