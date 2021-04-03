b7f8e40df503b9145d082f9aa7829d7f6f0cdfde
Jordan <jordmax12@gmail.com>
2021-04-02 22:37:56 -0500

    Update trainer.test.js

--- tests/v1/controller/trainer.test.js
+++ tests/v1/controller/trainer.test.js
@@ -22,19 +22,8 @@ describe('/v1/trainer', () => {
     expect(res.body.trainer.workouts.length).toBe(6);
   })
 
-  it('Valid Get Trainer by id', async () => {
+  it('Invalid Get Trainer by id', async () => {
     const res = await request(app).get("/v1/trainer?id=invalid").set( { api_key: API_KEY });
-    const expected = {
-      id: '1',
-      filming_datetime: '2021-03-29T07:35:25Z',
-      filming_duration: 100,
-      workout_name: 'test2',
-      workout_status: 'PLANNING',
-      workout_level: 'BEGINNER',
-      trainer_id: '1',
-      created: '2021-03-29T07:35:25Z',
-      modified: '2021-04-02T22:23:51.361Z'
-    }
 
     expect(res.body.message).toBe('trainer by id does not exist');
   })
